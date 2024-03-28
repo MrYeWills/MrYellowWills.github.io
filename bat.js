e9728a1e2b873d2be16b4334157ca27f1fc166c2
huangzhi <601661706@qq.com>
2024-03-29 00:52:02 +0800

    public path批量处理脚本,静态文件使用规范写法

---
+++ bat.js
@@ -0,0 +1,24 @@
+var fs = require('fs')
+var path=require('path');
+ 
+var filePath=path.resolve(__dirname+'/source/_posts');  
+//readdir方法读取文件名
+//readFile方法读取文件内容
+//writeFile改写文件内容
+fs.readdir(filePath, 'utf8', function (err,data) {
+ 
+	data.forEach(function(item, index) {
+		//console.log(item)
+		fs.readFile(filePath+'/'+item,'utf8',function(err,files){
+			//console.log(files)
+			// 将 ![](/image/infra/tab.png) 改成 {% img url_for /image/infra/tab.png %}
+			var result = files.replace(/!\[\]\((\/image[^)]+)\)/g, '{% img url_for $1 %}')
+ 
+			fs.writeFile(filePath+'/'+item, result, 'utf8', function (err) {
+			     if (err) return console.log(err);
+			});
+ 
+		})
+	});
+ 
+});
